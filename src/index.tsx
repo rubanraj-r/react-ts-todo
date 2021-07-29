ca68c6ece98f5cdf4c861bcdba288c7b0b2f23a0
rubanraj.rajdurai <rubanraj.r07@wipro.com>
2021-07-29 14:12:05 +0100

    ToDo app built

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import App from './app/App';
+import './index.css';
 
-ReactDOM.render(
+render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
